f0da29b5444de7eeabfac62e0ff38a5f9ef1f98a
javicod92 <javi.javier.jc@gmail.com>
2025-03-09 03:16:55 -0300

    Establishing English as the default  language

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
   const lang =
-    context.request.headers.get("cookie")?.match(/lang=(en|es)/)?.[1] || "es";
+    context.request.headers.get("cookie")?.match(/lang=(en|es)/)?.[1] || "en";
   (context.locals as { lang: string }).lang = lang;
   return next();
 });
