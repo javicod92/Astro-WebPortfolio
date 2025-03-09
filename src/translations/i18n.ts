a174bf660ee5bc154bedbdbdc5af6bc9e876d6ed
javicod92 <javi.javier.jc@gmail.com>
2025-03-09 16:09:27 -0300

    Finishing the translation of the technical English course to Spanish.

--- src/translations/i18n.ts
+++ src/translations/i18n.ts
@@ -1,8 +1,3 @@
-import PortfolioCoursesEn from "./portfolio/PortfolioCoursesEn.astro";
-import PortfolioCoursesEs from "./portfolio/PortfolioCoursesEs.astro";
-import PortfolioTitleEn from "./portfolio/PortfolioTitleEn.astro";
-import PortfolioTitleEs from "./portfolio/PortfolioTitleEs.astro";
-
 export const translations = {
   //Page title
   ROOT: {
@@ -120,15 +115,106 @@ export const translations = {
   //Portfolio section
   PORTFOLIO: {
     en: {
-      title: PortfolioTitleEn,
-      coursesCards: PortfolioCoursesEn,
-      buttonCertification: "View Certification",
+      h2: "Courses and Certifications",
+      h3: `These are some of my most relevant <span class="text-cyan-500"> courses and certifications </span>`,
+      coursesCards: [
+        {
+          title: "React",
+          platform: "Informatorio Chaco",
+          date: "July - November 2024",
+          href: "/Certifications/React-Informatorio.pdf",
+          description: `<span class="text-gray-200 text-pretty"> Developed and deployed <span class="HighLight" >interactive web applications</span> using <span class="HighLight">React and Next.js.</span> Implemented state management with <span class="HighLight">useState, useEffect, useRef</span> and consolidated the use of <span class="HighLight" >React Router, TypeScript, and Context API</span>. Integrated authentication with <span class="HighLight" >NextAuth and JWT</span>, connected to databases like <span class="HighLight">MongoDB</span>, and consumed APIs with <span class="HighLight">Axios</span>. Optimized styling with <span class="HighLight">Tailwind CSS</span> and enhanced user experience with <span class="HighLight">Framer Motion animations</span>. Applied these skills in personal projects, including an <span class="HighLight">audio streaming app</span> inspired by YouTube Music. </span>`,
+        },
+
+        {
+          title: "Inglés Técnico (Technical English)",
+          platform: "Informatorio Chaco",
+          date: "July - November 2024",
+          href: "/Certifications/InglesTecnico-Informatorio.pdf",
+          description: `<span class="text-gray-200 text-pretty"> Enhanced my <span class="HighLight">English proficiency</span> in reading, writing, and speaking, with a specific focus on <span class="HighLight"> technical terminology in programming</span >. Applied these skills in various projects to improve documentation, communication, and coding efficiency. </span>`,
+        },
+
+        {
+          title: "JavaScript Algorithms and Data Structures (Beta)",
+          platform: "freeCodeCamp",
+          date: "July 2024",
+          href: "/Certifications/JsDataStructures-freeCodeCamp.png",
+          description: `<span class="text-gray-200 text-pretty"> Strengthened my <span class="HighLight">JavaScript</span> skills by building <span class="HighLight">interactive applications</span> and improving my understanding of <span class="HighLight"> functions, arrays, objects, and asynchronous programming</span > . Gained experience in <span class="HighLight"> DOM manipulation, event handling,</span > and best practices for clean and maintainable code. </span>`,
+        },
+
+        {
+          title:
+            "Introducción a la Programación (Introduction to the Programming)",
+          platform: "Informatorio Chaco",
+          date: "May - June 2024",
+          href: "/Certifications/IntroduccionProgramacion-Informatorio.pdf",
+          description: `<span class="text-gray-200 text-pretty">Gained hands-on experience with <span class="HighLight"> HTML, CSS3, Git, and GitHub</span > by developing a personal portfolio website. Applied <span class="HighLight" > best programming practices</span > to improve code structure, maintainability, and accessibility. </span>`,
+        },
+
+        {
+          title: "Legacy JavaScript Algorithms and Data Structures",
+          platform: "freeCodeCamp",
+          date: "May - 2024",
+          href: "/Certifications/LegacyJs-freeCodeCamp.png",
+          description: `<span class="text-gray-200 text-pretty"> Strengthened my understanding of <span class="HighLight" >JavaScript</span > by working with <span class="HighLight" >variables, functions, and classes</span > . Improved my ability to perform <span class="HighLight"> arithmetic operations</span > and efficiently manipulate <span class="HighLight" >arrays and objects</span >. </span>`,
+        },
+
+        {
+          title: "Curso CSS3 (CSS3 Course)",
+          platform: "Ademass",
+          date: "May - 2024",
+          href: "/Certifications/CSS3-Ademass.pdf",
+          description: `<span class="text-gray-200 text-pretty"> Applied <span class="HighLight">CSS3</span> concepts in real projects, improving my skills in <span class="HighLight"> selectors, classes, and responsive design</span > . Focused on <span class="HighLight">Flexbox and Grid</span> for structuring layouts and developed <span class="HighLight" >animations and transitions</span > to enhance <span class="HighLight">user experience</span>. Also learned best practices for <span class="HighLight"> scalable and maintainable styles</span >. </span>`,
+        },
+
+        {
+          title: "Curso HTML5 (HTML5 Course)",
+          platform: "Ademass",
+          date: "May - 2024",
+          href: "/Certifications/HTML5-Ademass.pdf",
+          description: `<span class="text-gray-200 text-pretty">Learned to build <span class="HighLight">structured and accessible</span > web applications using <span class="HighLight">HTML5</span>. Focused on <span class="HighLight"> semantic elements, headings, forms, and attributes</span > to improve <span class="HighLight">SEO and user experience</span>. Gained experience with <span class="HighLight">ARIA roles</span> for accessibility and integrated multimedia elements like <span class="HighLight" >videos and audio</span > to create engaging content. </span>`,
+        },
+
+        {
+          title: "Responsive Web Design",
+          platform: "freeCodeCamp",
+          date: "April - 2024",
+          href: "/Certifications/ResponsiveWD-freeCodeCamp.png",
+          description: `<span class="text-gray-200 text-pretty"> Developed <span class="HighLight">responsive websites</span> using <span class="HighLight" > HTML5, CSS3, Flexbox, and Grid</span > , ensuring adaptability across different devices. Implemented <span class="HighLight" > media queries</span > to create fluid designs that adjust dynamically to screen sizes. Built several <span class="HighLight">real-world projects</span> to solidify my skills in modern <span class="HighLight">web design and accessibility</span>. </span>`,
+        },
+
+        {
+          title: "Desarrollo Web (Web Development)",
+          platform: "Informatorio Chaco",
+          date: "August - December 2022",
+          href: "/Certifications/DesarrolloWeb-Informatorio.png",
+          description: `<span class="text-gray-200 text-pretty"> Learned and applied backend oriented programming skills using <span class="HighLight">Python and Django.</span> Developed the user interface for an “association” using HTML CSS and Vanilla JavaScript. Implemented user authentication and user storage in <span class="HighLight"> SQL Server and SQLite3</span > databases. This was one of my first experiences for a real client. </span>`,
+        },
+      ],
+      certificationButton: "View Certification",
     },
 
     es: {
-      title: PortfolioTitleEs,
-      coursesCards: PortfolioCoursesEs,
-      buttonCertification: "Ver Certificación",
+      h2: "Cursos y Certificaciones",
+      h3: `Estos son algunos de mis más relevantes<span class="text-cyan-500"> cursos y certificaciones </span>`,
+      coursesCards: [
+        {
+          title: "React",
+          platform: "Informatorio Chaco",
+          date: "Julio - Noviembre 2024",
+          href: "/Certifications/React-Informatorio.pdf",
+          description: `<span class="text-gray-200 text-pretty"> Desarrollé y desplegué <span class="HighLight" >aplicaciones web interactivas</span > usando <span class="HighLight">React y Next.js.</span> Implementé el manejo de estados con <span class="HighLight">useState, useEffect, useRef</span> y consolidé el uso de <span class="HighLight">React Router, TypeScript, y Context API</span>. Integré autenticación con <span class="HighLight">NextAuth y JWT</span>, conecté a base de datos como <span class="HighLight">MongoDB</span> y consumí APIs con <span class="HighLight"> Axios</span>. Optimice estilos con <span class="HighLight">Tailwind CSS</span> y mejoré la experiencia de usuario con <span class="HighLight">Framer Motion animations</span>. Apliqué estas habilidades en proyectos personales, incluyendo una <span class="HighLight" > aplicación de reproducción de audio</span > inspirada en YouTube Music. </span>`,
+        },
+
+        {
+          title: "Inglés Técnico",
+          platform: "Informatorio Chaco",
+          date: "Julio - Noviembre 2024",
+          href: "/Certifications/InglesTecnico-Informatorio.pdf",
+          description: `<span class="text-gray-200 text-pretty"> Mejoré mi <span class="HighLight">dominio de Inglés</span> en lectura, escritura y habla, con un enfoque especifico sobre <span class="HighLight"> terminologías técnicas en programación.</span > Apliqué estas habilidades en multiples proyectos para mejorar documentación, comunicación y eficiencia del código. </span>`,
+        },
+      ],
+      certificationButton: "Ver Certificación",
     },
   },
 };
